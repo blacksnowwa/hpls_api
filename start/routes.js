ec931767fb2ed36347f7d111140e147d278b077e
อธิวัตร เชยโนนสูง <atiwat.cns@gmail.com>
2021-07-16 21:24:12 +0700

    crud item

--- start/routes.js
+++ start/routes.js
@@ -13,28 +13,26 @@ Route.group(() => {
   Route.route("get", "UserController.fetch", ["GET", "POST"]);
 }).prefix("user");
 Route.group(() => {
-  Route.post('login', 'Auth/AuthenticationController.login')
-  Route.post('register', 'Auth/AuthenticationController.register')
-  Route.get('me', 'Auth/AuthenticationController.me').middleware(['auth'])
-}).prefix('api')
-Route.get('api/daily', 'DialyController.getDialy')
-Route.get('api/daily/:id', 'DialyController.getDialy')
+  Route.post("login", "Auth/AuthenticationController.login");
+  Route.post("register", "Auth/AuthenticationController.register");
+  Route.get("me", "Auth/AuthenticationController.me").middleware(["auth"]);
+}).prefix("api");
+Route.get("api/daily", "DialyController.getDialy");
+Route.get("api/daily/:id", "DialyController.getDialy");
 
 Route.group(() => {
-  Route.get('', 'ItemController.getitem')
-  Route.post('', 'ItemController.create')
-  Route.post('/:name', 'ItemController.create')
-  Route.get('/loop', 'ItemController.loop')
-
-}).prefix('api/items')
+  Route.get("", "ItemController.getitem");
+  Route.post("create", "ItemController.create");
+  Route.post("update", "ItemController.update");
+  Route.post("delete", "ItemController.delete");
+  Route.post("/:name", "ItemController.createByParams");
+  Route.get("/loop", "ItemController.loop");
+}).prefix("api/items");
 Route.group(() => {
-  Route.get('', 'StatementController.getall')
-  Route.post('saveall', 'StatementController.saveall')
-
-
-}).prefix('api/statement')
+  Route.get("", "StatementController.getall");
+  Route.post("saveall", "StatementController.saveall");
+}).prefix("api/statement");
 Route.group(() => {
-  Route.get('/:username', 'ConfigController.getConfig')
-  Route.post('create', 'ConfigController.createConfig')
-
-}).prefix('api/config')
+  Route.get("/:username", "ConfigController.getConfig");
+  Route.post("create", "ConfigController.createConfig");
+}).prefix("api/config");
